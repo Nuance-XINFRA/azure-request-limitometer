7213e26f55c97c6a151f0c57efd6c904d878b26c
Gilles Dubuc <gilles.dubuc@nuance.com>
2019-03-18 16:57:56 -0400

    Support for national cloud authentication with MSI

--- pkg/common/types.go
+++ pkg/common/types.go
@@ -3,29 +3,35 @@ package common
 // ComputeInstanceMetadata denotes the compute specific instance metadata from Azure Instance
 // Metadata Service
 type ComputeInstanceMetadata struct {
-	Location             string `json:"location,omitempty"`
-	Name                 string `json:"name,omitempty"`
-	Offer                string `json:"offer,omitempty"`
-	OsType               string `json:"osType,omitempty"`
-	PlacementGroupID     string `json:"placement_group_id,omitempty"`
-	PlatformFaultDomain  string `json:"platformFaultDomain,omitempty"`
-	PlatformUpdateDomain string `json:"platformUpdateDomain,omitempty"`
-	Publisher            string `json:"publisher,omitempty"`
-	ResourceGroupName    string `json:"resourceGroupName,omitempty"`
-	Sku                  string `json:"sku,omitempty"`
-	SubscriptionID       string `json:"subscriptionId,omitempty"`
-	Tags                 string `json:"tags,omitempty"`
-	Version              string `json:"version,omitempty"`
-	VMID                 string `json:"vmId,omitempty"`
-	VMScaleSetName       string `json:"vmScaleSetName,omitempty"`
-	VMSize               string `json:"vmSize,omitempty"`
-	Zone                 string `json:"zone,omitempty"`
+	Environment          string              `json:"azEnvironment,omitempty"`
+	Location             string              `json:"location,omitempty"`
+	Name                 string              `json:"name,omitempty"`
+	Offer                string              `json:"offer,omitempty"`
+	OsType               string              `json:"osType,omitempty"`
+	PlacementGroupID     string              `json:"placement_group_id,omitempty"`
+	Plan                 map[string]string   `json:"plan,omitempty"`
+	PlatformFaultDomain  string              `json:"platformFaultDomain,omitempty"`
+	PlatformUpdateDomain string              `json:"platformUpdateDomain,omitempty"`
+	Provider             string              `json:"provider,omitempty"`
+	PublicKeys           []map[string]string `json:"publicKeys,omitempty"`
+	Publisher            string              `json:"publisher,omitempty"`
+	ResourceGroupName    string              `json:"resourceGroupName,omitempty"`
+	Sku                  string              `json:"sku,omitempty"`
+	SubscriptionID       string              `json:"subscriptionId,omitempty"`
+	Tags                 string              `json:"tags,omitempty"`
+	Version              string              `json:"version,omitempty"`
+	VMID                 string              `json:"vmId,omitempty"`
+	VMScaleSetName       string              `json:"vmScaleSetName,omitempty"`
+	VMSize               string              `json:"vmSize,omitempty"`
+	Zone                 string              `json:"zone,omitempty"`
 }
 
 // Config returns a config with the Azure resource group and Azure location to perform requests
 type Config struct {
-	VMName         string
-	SubscriptionID string
-	Location       string
-	ResourceGroup  string
+	VMName              string
+	SubscriptionID      string
+	Location            string
+	ResourceGroup       string
+	AzureEnvironment    string
+	EnvironmentEndpoint string
 }
